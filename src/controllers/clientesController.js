5ded212456bb31ede903e305e9b6c0fbcc21f728
Marilia Aparecida Messias Oliveira <marilia.oliveira@terceiro-sky.com.br>
2019-11-14 14:01:22 -0300

    initial commit

---
+++ src/controllers/clientesController.js
@@ -0,0 +1,46 @@
+const Clientes = require('../model/clientes');
+
+exports.get = (req, res) => {
+    Clientes.find(function (err, clientes) {
+        if (err) res.status(500).send(err);
+        res.status(200).send(clientes);
+    })
+}
+
+exports.post = function (req, res) {
+    let cliente = new Clientes(req.body);
+
+    cliente.save(function (err) {
+        if (err) res.status(500).send(err);
+        else {
+            res.status(201).send({
+                status: true,
+                mensagem: "Aluna incluida com sucesso"
+            });
+        }
+    });
+}
+
+exports.getCompradores = (req, res) => {
+    Clientes.find(function (err, clientes) {
+        if (err) res.status(500).send(err);
+
+        const clientesCompradores = clientes.filter(cliente => cliente.comprou == true )
+        const clientesRetorno = clientesCompradores.map(cliente => {
+            return {
+               nome: cliente.nome,
+               email: cliente.email
+            }
+        })
+        
+        res.status(200).send(clientesRetorno);
+    })
+}
+exports.getByCpf = (req, res) => {
+    const cpf = req.params.cpf;
+    Clientes.find({ cpf }, function (err, cliente) {
+        if (err) res.status(500).send(err);        
+        res.status(200).send(cliente);
+    })
+}
+
---
+++ src/routes/clientesRoute.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/clientesController")
+
+router.get("/", controller.get)
+router.get("/compradores", controller.getCompradores)
+router.get("/:cpf", controller.getByCpf)
+router.post("/", controller.post)
+
+module.exports = router
